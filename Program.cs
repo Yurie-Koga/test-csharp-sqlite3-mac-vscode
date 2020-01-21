27d4744ba45e639e4bb5ff57c776f7a657ce6854
Yurie <yurie.koga.a@gmail.com>
2020-01-20 22:32:54 -0800

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace test_csharp_sqlite3_mac_vscode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
